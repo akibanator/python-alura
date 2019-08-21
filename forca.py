ff1eb860900a7894d4e9300085a3f666b87a7740
Ailton Kenji Akiba <ailtonkenji0@gmail.com>
2019-08-21 05:15:43 -0300

    Organization code improved

--- forca.py
+++ forca.py
@@ -17,15 +17,10 @@ def jogar():
 
         print("Você ainda tem {} tentativas.".format(erros))
         
-        chute = input("Qual letra? ")
-        chute = chute.strip().upper()
+        chute = pede_chute()
 
         if chute in palavra_secreta:
-                index = 0
-                for letra in palavra_secreta:
-                        if (chute == letra):
-                                letras_acertadas[index] = letra
-                        index = index + 1
+                marca_chute_correto(palavra_secreta, chute, letras_acertadas)
         else:
                 erros -= 1
 
@@ -37,9 +32,9 @@ def jogar():
         acertou = "_" not in letras_acertadas
 
     if (acertou):
-                print("Você ganhou!!!!")
+                imprime_mensagem_ganhador()
     else:
-                print("Você perdeu!!!!")
+                imprime_mensagem_perdedor()
 
 def imprime_mensagem_abertura():
     print("---------------------------")
@@ -64,6 +59,24 @@ def mostra_qtd_letras(letras_acertadas):
     letras_faltando = str(letras_acertadas.count("_"))
     print(letras_acertadas)
     print("Ainda faltam acertar {} letras".format(letras_faltando))
+
+def pede_chute():
+    chute = input("Qual letra? ")
+    chute = chute.strip().upper()
+    return chute
+
+def marca_chute_correto(palavra_secreta, chute, letras_acertadas):
+        index = 0
+        for letra in palavra_secreta:
+                if (chute == letra):
+                        letras_acertadas[index] = letra
+                index = index + 1
+
+def imprime_mensagem_ganhador():
+        print("Você ganhou!!!!")
+
+def imprime_mensagem_perdedor():
+        print("Você perdeu!!!!")
 
 if (__name__ == "__main__"):
     jogar()
